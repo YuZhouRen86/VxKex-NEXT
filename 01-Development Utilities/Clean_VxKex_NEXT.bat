a61b906acf7c0ed96b2b48c6eec9bef2b25c9e11
YuZhouRen86 <you@example.com>
2026-07-30 14:01:43 +0800

    Update Clean_VxKex_NEXT.bat

--- 01-Development Utilities/Clean_VxKex_NEXT.bat
+++ 01-Development Utilities/Clean_VxKex_NEXT.bat
@@ -11,7 +11,7 @@ for /F "delims=" %%d in ('dir /AD /B /S 2^>nul') do (
     )
 )
 
-for /F "delims=" %%f in ('dir /A-D /B /S *.aps *.cache *.sdf *.suo *.user *.VC.db *.VC.opendb 2^>nul') do if exist "%%f" echo Deleting file "%%f" && del /A /F /Q "%%f"
+for /F "delims=" %%f in ('dir /A-D /B /S debug.log *.aps *.cache *.sdf *.suo *.user *.VC.db *.VC.opendb 2^>nul') do if exist "%%f" echo Deleting file "%%f" && del /A /F /Q "%%f"
 for /F "delims=" %%f in ('dir /A-D /B /S "02-Prebuilt Data\*.pdb" 2^>nul') do if exist "%%f" echo Deleting file "%%f". && del /A /F /Q "%%f"
 for /F "delims=" %%f in ('dir /A-D /B /S "02-Prebuilt Data\KexDir\Globalization\Dictionaries\*" 2^>nul') do if exist "%%f" echo Deleting file "%%f". && del /A /F /Q "%%f"
 if exist "01-Development Utilities\moz2sst\moz2sst.exe" del /A /F /Q "01-Development Utilities\moz2sst\moz2sst.exe"
